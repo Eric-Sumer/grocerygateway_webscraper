62fe8f5192fd340c3b07995401f81dac085d445e
Alysium <chang.li@live.com>
2021-10-11 23:26:06 -0400

    Added Image Downloads + Fixed Product Listing Page Bug

--- generalWebscraper_v2.py
+++ generalWebscraper_v2.py
@@ -5,9 +5,12 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+import requests
 import math
 import time
 import csv
+import os
+import sys
 
 class GeneralWebscraper:
     def __init__(self, csvFileName):
@@ -20,6 +23,7 @@ class GeneralWebscraper:
         self.fieldNames = ['UPC', 'Product Name', 'Category', 'Subcategory', 'In Stock', 'Price', 'Sale', 'Sale Price', 'Unit', 'Image Link', 'Description', 'Product Page Link']
         self.products = []
         self.csvPath = './CsvFiles/'
+        self.productImagePath = './ProductImages/'
 
 
     def loadCategoryPage(self, url):
@@ -33,20 +37,18 @@ class GeneralWebscraper:
             return []
         try: 
             #get product boxes
-            prodPerRefresh = 24
-            showing_of_string_arr = self.driver.find_element_by_xpath("//div[@class='d-flex d-xl-none filters-items-counter']").text.split(" ")
-            totalProds = int(showing_of_string_arr[3])
-            numPresses = math.ceil(max(0,totalProds-prodPerRefresh)/prodPerRefresh)
-            for _ in range(numPresses):
+            while self.driver.find_elements_by_xpath("//button[@class='btn-black']"):
                 loadMoreBtn = self.driver.find_element_by_xpath("//button[@class='btn-black']")
                 self.driver.execute_script("arguments[0].click()", loadMoreBtn)
-                time.sleep(1)
+                time.sleep(2)
+
             time.sleep(5)
             parentBoxes = self.driver.find_elements_by_xpath("//div[@class='product-list-item_img-holder text-center']")
             return [parentBox.find_element_by_tag_name("a").get_attribute("href") for parentBox in parentBoxes]
         except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
             print("Error occured for category at url", url)
-            print("Error is ", e)
+            print("Error is ", e, exc_tb.tb_lineno)
             print("Continuing Run")
             return []
             
@@ -55,6 +57,22 @@ class GeneralWebscraper:
             return ""
         lengthOfUPC = 14
         return ('0'*max(0,(lengthOfUPC-len(upcText))))+upcText
+
+    def downloadImage(self, link, parentCat, subCat):
+        img_data = requests.get(link).content
+        image_link = link.split("medias/")[-1]
+        if '.png?' in image_link:
+            image_link = image_link.split(".png")[0]+".png"
+        elif '.jpg?' in image_link:
+            image_link = image_link.split(".jpg")[0]+".jpg"
+        else:
+            return False
+        subFolderName = (parentCat +"_"+ subCat).replace(" ", "_")
+        if not os.path.isdir(self.productImagePath+subFolderName):
+            os.mkdir(self.productImagePath+subFolderName)
+        with open(self.productImagePath+subFolderName+"/"+image_link,'wb') as handler:
+            handler.write(img_data)
+        return True
 
     def scrapeProductPage(self,url, category, parentCategory):
         try:
@@ -100,6 +118,9 @@ class GeneralWebscraper:
                 originalPrice = priceElement.find_element_by_xpath("//span[@class='font-weight-bold']").get_attribute("textContent")[1:]+"."+\
                     priceElement.find_element_by_xpath("//span[@class='cents font-weight-bold']").get_attribute("textContent")
 
+            downloadImageSuccessful = self.downloadImage(imageUrl, parentCategory, category)
+            if not downloadImageSuccessful:
+               return {}
             return {
                 'UPC':upc,
                 'Product Name':title,
@@ -128,17 +149,19 @@ class GeneralWebscraper:
             if listEle != {} : self.products.append(listEle)
         self.writeCSV(parentCat,subCat)
 
+    def clearProducts(self):
+        self.products = []
+
     def writeCSV(self, parentCat, subCat):
         fileName = (self.csvPath+parentCat+"_"+subCat+".csv").replace(" ", "_")
         with open(fileName, 'w') as csvfile:
-            filewriter = csv.DictWriter(csvfile, fieldnames=self.fieldNames)
+            filewriter = csv.DictWriter(csvfile, fieldnames=self.fieldNames, quoting=csv.QUOTE_ALL)
             filewriter.writeheader()
             for productDict in self.products:
                 if type(productDict)!= dict:
                     continue
                 filewriter.writerow(productDict)
-    def clearProducts(self):
-        self.prodcuts = []
+        self.clearProducts()
 
 if __name__ == "__main__":
     csvFileName = "productList.csv"
@@ -150,10 +173,11 @@ if __name__ == "__main__":
     skip = 1
     breakCnt = float('inf')
     for line in lines:
-        cnt+=1
-        if cnt <= skip: continue
+        if cnt < skip: 
+            cnt+=1
+            continue
         if cnt > breakCnt: break 
         parentCat,subCat,url = line.split("|||")
         print("Processing", url)
-        webScraper.clearProducts()
         webScraper.scrapeCategoryPage(parentCat,subCat,url)
+        cnt+=1
